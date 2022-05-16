f101a48ced86ca4b3a0907fd61fbed621cea3762
slavakukuyev <slava.kukuyev@gmail.com>
2022-05-16 22:25:37 +0300

    replace frameborder with frameBorder (react style)

--- src/DataSourceIFrame.js
+++ src/DataSourceIFrame.js
@@ -1,6 +1,4 @@
-import React, { Component, PropTypes } from 'react'
-import ReactDOM from 'react-dom'
-
+import React, { Component } from 'react'
 
 export default class DataSourceIFrame extends Component {
     render() {
@@ -8,7 +6,7 @@ export default class DataSourceIFrame extends Component {
             <iframe width={"100%"} 
             height={"100%"} 
             src={"https://datastudio.google.com/embed/reporting/0B5FF6JBKbNJxOWItcWo2SVVVeGc/page/MjD"} 
-            frameborder={0} 
+            frameBorder={0} 
             style={{"border":"0", position:"absolute", top:0, left:0,alignSelf:"center"}} 
             allowFullScreen />
         );
